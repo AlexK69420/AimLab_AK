29d504ab867f1c256bd1167a2178eaea20d4216c
tobias-htl-wo <tobias.schober@htl-wolfsberg.at>
2023-09-20 14:34:24 +0200

    FirstMovement

---
+++ AimLab_AK/Assets/Player/Player Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.position += transform.forward * vertical;
+
+        transform.position += transform.right * horizontal;
+    }
+}
